8e63cf4f45fd5adce90b925a710a1cba792abd8a
氘化氢 <36532302+HydrogenDeuterium@users.noreply.github.com>
2021-12-08 16:43:24 +0800

    脑补调底盘

--- controller/chassis.py
+++ controller/chassis.py
@@ -10,54 +10,50 @@ from drivers.motor import Motor
 class Chassis:
     def __init__(self):
         """
-        电机控制使用 35-38 针脚
+        电机控制使用针脚
         按照顺序分别为左前右前左后右后
         """
-        # TODO 电机要两根针脚控制
-        self.__motor1 = Motor(35)
-        self.__motor2 = Motor(36)
-        self.__motor3 = Motor(37)
-        self.__motor4 = Motor(38)
+        self.__motor1 = Motor(29, 31)
+        self.__motor2 = Motor(32, 36)
+        self.__motor3 = Motor(33, 35)
+        self.__motor4 = Motor(38, 40)
         
         self.__motors: List[Motor] = [self.__motor1, self.__motor2, self.__motor3, self.__motor4]
     
-    def go(self, distance):
-        ms_to_run_per_meter = 1000
+    def go(self, distance_meter):
+        secs_to_run_per_meter = 3
+        time_to_run = distance_meter * secs_to_run_per_meter
         [motor.start_rotate() for motor in self.__motors]
-        time.sleep(distance * ms_to_run_per_meter)
+        time.sleep(time_to_run)
         [motor.stop_rotate() for motor in self.__motors]
     
-    def back(self, distance):
-        ms_to_run_per_meter = 1000
+    def back(self, distance_meter):
+        secs_to_run_per_meter = 3
+        time_to_run = distance_meter * secs_to_run_per_meter
         [motor.start_rotate_reverse() for motor in self.__motors]
-        time.sleep(distance * ms_to_run_per_meter)
+        time.sleep(time_to_run)
         [motor.stop_rotate() for motor in self.__motors]
-        pass
     
-    def left(self, degree=90):
-        ms_to_run_per_meter = 10
+    def left(self, angle_degree=90):
+        sec_to_run_per_degree = 0.05
         
         self.__motor1.start_rotate_reverse()
-        self.__motor2.start_rotate()
         self.__motor3.start_rotate_reverse()
+        self.__motor2.start_rotate()
         self.__motor4.start_rotate()
         
-        time.sleep(degree * ms_to_run_per_meter)
+        time.sleep(angle_degree * sec_to_run_per_degree)
         
         [motor.stop_rotate() for motor in self.__motors]
-        pass
     
     def right(self, degree=90):
-        ms_to_run_per_meter = 10
+        ms_to_run_per_meter = 0.05
         
         self.__motor1.start_rotate()
-        self.__motor2.start_rotate_reverse()
         self.__motor3.start_rotate()
+        self.__motor2.start_rotate_reverse()
         self.__motor4.start_rotate_reverse()
         
         time.sleep(degree * ms_to_run_per_meter)
         
         [motor.stop_rotate() for motor in self.__motors]
-        pass
-    
-    pass
--- controller/drivers/motor.py
+++ controller/drivers/motor.py
@@ -1,17 +1,25 @@
+from RPi import GPIO
+
+
 class Motor:
-    def __init__(self, pin):
-        # TODO 电机要两根针脚控制
-        self.pin = pin
+    def __init__(self, pin1, pin2, ratio_duty=100):
+        """正转针脚，倒转针脚，pwm修正率(%)"""
+        GPIO.setup(pin2, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(pin1, GPIO.OUT, initial=GPIO.LOW)
+        self.pin1 = pin1
+        self.pin2 = pin2
     
     def start_rotate(self):
         """设置正车"""
-        # TODO
-        pass
+        GPIO.output(self.pin1, GPIO.HIGH)
+        GPIO.output(self.pin2, GPIO.LOW)
     
     def start_rotate_reverse(self):
         """设置倒车"""
-        pass
+        GPIO.output(self.pin1, GPIO.LOW)
+        GPIO.output(self.pin2, GPIO.HIGH)
     
     def stop_rotate(self):
         """设置停车"""
-        pass
+        GPIO.output(self.pin1, GPIO.LOW)
+        GPIO.output(self.pin2, GPIO.LOW)
